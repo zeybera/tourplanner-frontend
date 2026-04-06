e19ecffb893fe38b33d42fe812f07fd9631ca53a
zeybera <zeyneb_tokel@hotmail.com>
2026-04-06 23:10:25 +0200

    refactor navbar folder

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar';
+import { NavbarComponent } from './shared/navbar/navbar';
 
 @Component({
   selector: 'app-root', //In index.html <app-root></app-root> and replaces it with this component. That is how your app appears in the browser.
   imports: [RouterOutlet, NavbarComponent],
-  templateUrl: './app.html', // templateUrl points to the HTML file of this component. This file usually contains a <router-outlet>. 
+  templateUrl: './app.html', // templateUrl points to the HTML file of this component. This file usually contains a <router-outlet>.
   // router-outlet is placeholder and it is the place where Angular dynamically renders components based on the current route.
   styleUrl: './app.css'
 })
@@ -13,4 +13,4 @@ export class App {
   protected readonly title = signal('tourplanner');
 }
 
-// AppComponent 
+// AppComponent

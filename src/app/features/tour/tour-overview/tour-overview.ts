504836b03dacf34f1d2cdf5996c65f63823b839e
zeybera <zeyneb_tokel@hotmail.com>
2026-04-06 19:33:05 +0200

    implement: tour overview and map as placeholder

---
+++ src/app/features/tour/tour-overview/tour-overview.ts
@@ -0,0 +1,17 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TourListComponent } from '../tour-list';
+import { TourService } from '../tour.service';
+
+@Component({
+  selector: 'app-tour-overview',
+  standalone: true,
+  imports: [CommonModule, TourListComponent],
+  templateUrl: './tour-overview.html',
+  styleUrls: ['./tour-overview.css']
+})
+export class TourOverviewComponent {
+
+  service = inject(TourService);
+
+}

a008eb1581f196f7599ed23e458fb517ffd36111
zeybera <zeyneb_tokel@hotmail.com>
2026-04-12 21:08:28 +0200

    update service

--- src/app/features/tour/tour.service.ts
+++ src/app/features/tour/tour.service.ts
@@ -6,29 +6,29 @@ import { Tour } from './tour.model';
 })
 
 export class TourService {
-  //inside this signal there is an array of tours
   // private bedeutet: signal wird nur im Service erreichbar, zb. außerhalb "this.service._tours" nicht erreichbar
   // Wie wird der State trotzdem verändert?  Nicht direkt, sondern über Methoden im Service. Komponenten greifen nicht direkt auf das Signal zu, sondern rufen Funktionen im Service auf.
   private _tours = signal<Tour[]>([]);
   // warum brauchen wir readonly signal? Components den State sehen, aber NICHT verändern könne
-  // Component darf nur: this.service.tours() für lesen aufrufen, aber nicht this.service.tours.set() oder this.service.tours.update() 
+  // Component darf nur: this.service.tours() für lesen aufrufen, aber nicht this.service.tours.set() oder this.service.tours.update()
   readonly tours = this._tours.asReadonly();
 
   private nextId = 15;
 
   //TourService wird erzeugt, constructor wird ausgeführt, loadTours() wird aufgerufen
   //Der constructor wird ausgeführt, wenn das Objekt erstellt wird.
+
   constructor() {
     this.loadTours();
   }
 
   async loadTours() {
-  //TO DO: load from backend
-  const data : Tour[] = await fetch('../../../assets/tours.json').then(res => res.json());
- console.log("loading tours from json");
-  this._tours.set(data);
+    //TO DO: load from backend
+    const data : Tour[] = await fetch('../../../assets/tours.json').then(res => res.json());
+    console.log("loading tours from json");
+    this._tours.set(data);
 
-}
+  }
 
   // CREATE
 
@@ -56,35 +56,40 @@ export class TourService {
 
 
 // shared state across components, so we keep in service
-selectedId = signal<number | null>(null);
+  private _selectedId = signal<number | null>(null);
+  readonly selectedId = this._selectedId.asReadonly();
+
+  selectTour(id: number){
+    this._selectedId.set(id);
+  }
 
 //derived state
-selectedTour = computed(() => {
-  const id = this.selectedId();
-  if (id == null) {
-    return null;
-  }
-  const tour = this._tours().find(tour => tour.id == id);
-  if (tour == undefined) {
-    return null;
-  }
-  return tour;
-});
+  selectedTour = computed(() => {
+    const id = this.selectedId();
+    if (id == null) {
+      return null;
+    }
+    const tour = this._tours().find(tour => tour.id == id);
+    if (tour == undefined) {
+      return null;
+    }
+    return tour;
+  });
 
 // UPDATE
   //maybe partial for update
- update(updated: Tour): void {
-  this._tours.update(tours => {
-    const newTours: Tour[] = [];
-    for (let t of tours) {
-      if (t.id == updated.id) {
-        newTours.push(updated);
-      } else {
-        newTours.push(t);
+  update(updated: Tour): void {
+    this._tours.update(tours => {
+      const newTours: Tour[] = [];
+      for (let t of tours) {
+        if (t.id == updated.id) {
+          newTours.push(updated);
+        } else {
+          newTours.push(t);
+        }
       }
-    }
-    return newTours;
-  });
-}
+      return newTours;
+    });
+  }
 
 }

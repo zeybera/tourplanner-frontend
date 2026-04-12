90cdf13fca5cd39f5c20e67db339acc29be6e784
zeybera <zeyneb_tokel@hotmail.com>
2026-04-12 17:40:10 +0200

    Enhance tour log form: add date/time inputs, realistic validation and fix update logic

--- src/app/features/tour-log/tour-log-form/tour-log-form.ts
+++ src/app/features/tour-log/tour-log-form/tour-log-form.ts
@@ -69,22 +69,47 @@ export class TourLogFormComponent {
     const log = this.service.selectedLog();
 
     if(log) {
+      const [datePart, timePart] = log.date.split('T');
+
+      this.date.set(datePart);
+      this.time.set(timePart?.substring(0,5) || '');
+
       this.comment.set(log.comment);
       this.difficulty.set(log.difficulty);
       this.rating.set(log.rating);
+
+      this.totalDistance.set(log.totalDistance);
+      this.totalTime.set(log.totalTime);
     }
     });
 
 
-  isValid = computed(() =>
+  isValid = computed(() => {
+
+    const selectedDateTime = new Date(this.date() + 'T' + this.time());
+    const now = new Date();
+    const minDate = new Date('2020-01-01');
+
+    return(
     this.date() !== '' &&
     this.time() !== '' &&
+
+    selectedDateTime <= now &&
+    selectedDateTime >=minDate &&
+
     this.comment().trim() !== '' &&
     this.difficulty() >= 1 && this.difficulty() <= 5 &&
     this.rating() >= 1 && this.rating() <= 5 &&
+
+    // validation for total distance
     this.totalDistance() > 0 &&
-    this.totalTime() > 0
-  );
+    this.totalDistance() < 2000 && //max km
+
+    //validation for total time
+    this.totalTime() > 0 &&
+    this.totalTime() <= 1440 //max 24h
+    );
+  });
 
   create(): void {
 
@@ -98,9 +123,15 @@ export class TourLogFormComponent {
     if (existing) {
       this.service.update({
         ...existing,
+
+        date: this.date() + 'T' + this.time,
+
         comment: this.comment(),
         difficulty: this.difficulty(),
-        rating: this.rating()
+        rating: this.rating(),
+
+        totalDistance: this.totalDistance(),
+        totalTime: this.totalTime()
       });
     } else {
       this.service.create({
@@ -118,4 +149,29 @@ export class TourLogFormComponent {
 
     this.router.navigate(['/logs']);
   }
+
+
+
+  dateError = computed(() => {
+    if (this.date() === '' || this.time() === '') return 'Please select date and time';
+
+    const selected = new Date(this.date() + 'T' + this.time());
+    const now = new Date();
+
+    if (selected > now) return 'Date cannot be in the future';
+
+    return '';
+  });
+
+  distanceError = computed(() => {
+    if (this.totalDistance() <= 0) return 'Distance must be greater than 0';
+    if (this.totalDistance() > 2000) return 'Distance is too large';
+    return '';
+  });
+
+  timeError = computed(() => {
+    if (this.totalTime() <= 0) return 'Time must be greater than 0';
+    if (this.totalTime() > 1440) return 'Max duration is 24h';
+    return '';
+  });
 }
